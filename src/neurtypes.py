fb684c6fe1275364ab9d587ef11fb99ba318b7e0
Igglyn <igglynchan@gmail.com>
2026-02-14 16:17:02 -0800

    updating some code with comments

--- src/neurtypes.py
+++ src/neurtypes.py
@@ -1,50 +1,6 @@
-from typing import Annotated, Self, TypeAlias, Literal
+from typing import Annotated, TypeAlias, Literal
 
 import numpy as np
-
-class Rdu:
-    __array_priority__ = 1000
-    def __init__(self, axis:int|tuple[int]=-1):
-        self.axis = axis
-    def __rand__(self, left:np.ndarray) -> Self:
-        self.l = left
-        self.f = 0
-        return self
-    def __ror__(self, left:np.ndarray) -> Self:
-        self.l = left
-        self.f = 1
-        return self
-
-    def __eq__(self, right:np.ndarray) -> np.ndarray: # pyright: ignore[reportIncompatibleMethodOverride]
-        if self.f:
-            return np.bitwise_or.reduce(array=self.l, axis=self.axis, where=right)
-        else:
-            return np.bitwise_and.reduce(array=self.l, axis=self.axis, where=right)
-
-
-
-def case_gen_64(dtype:np.dtype[np.unsignedinteger]|type[np.unsignedinteger]) -> np.ndarray:
-    size = np.iinfo(dtype).bits
-
-    arrs = [np.arange(0,n, dtype=dtype) for n in range(size-1, 0, -1)]
-    arr2 = [np.array([n] * n, dtype=dtype) for n in range(size-1, 0, -1)]
-
-    ones1 = np.ones(((size) * (size-1)) // 2, dtype=dtype)
-    idx = np.concat(arrs)
-    idx2 = np.concat(arr2)
-    ones2 = np.copy(ones1)
-
-    cases = ones1 << idx | ones2 << idx2
-
-    cases_rolled_list = [cases[0]]
-    i = size
-    while i != 0:
-        cases_rolled_list.append(cases[i])
-        i = (i + size + 1) % ((size) * (size-1) // 2)
-    cases_rolled = np.array(cases_rolled_list)
-    return cases_rolled
-
-
 
 Case: TypeAlias = np.ndarray[tuple[int], np.dtype[np.unsignedinteger]]
 
--- src/tests.py
+++ src/tests.py
@@ -4,21 +4,21 @@ from neurrayv4 import U1XToU1X
 
 def sanity_test():
 
-    eliv = U1XToU1X(np.empty(4, dtype=np.uint8), np.uint8, cases=6)
+    eliv = U1XToU1X(np.empty(4, dtype=np.uint8), cases=6)
 
     temp = np.array([[8, 0, 0, 0], [2,0,0,0], [4,0,0,0]], dtype=np.uint8)
-    print(temp.shape)
 
-    res, rev = eliv.forward(temp)
+    rev = eliv.forward(temp)
     eliv.assign(rev)
+    print(eliv.array_used) # 3 cases
 
-    res, rev = eliv.forward(np.array([[9, 0, 0, 0], [12,0,0,0], [14,0,0,0]], dtype=np.uint8))
+    rev = eliv.forward(np.array([[9, 0, 0, 0], [12,0,0,0], [14,0,0,0]], dtype=np.uint8))
     eliv.assign(rev)
+    print(eliv.array_used) # 4 cases (adding [1, 0, 0, 0])
 
-    res, rev = eliv.forward(np.array([[9, 0, 0, 0], [12,0,0,0], [14,0,0,0]], dtype=np.uint8))
-    print(res)
+    rev = eliv.forward(np.array([[9, 0, 0, 0], [12,0,0,0], [14,0,0,0]], dtype=np.uint8))
+    print(eliv.array_used) # 4 cases (It's identical after all)
 
-    print(eliv.match, eliv.array_used)
 
 
 def dataset():
@@ -40,7 +40,7 @@ def dataset():
     x_train, y_train = train_np
     x_test, y_test   = test_np
 
-    def chunk_mnist_for_U1X(x):
+    def chunk_mnist_for_U1X(x: np.ndarray) -> np.ndarray:
         """
         x: numpy array of shape (N, 28, 28)
         returns: (N, 4, 4, 7, 7)
@@ -66,6 +66,13 @@ def dataset():
 
         return tiles
 
+    def recast_u64(a:np.ndarray) -> np.ndarray:
+        a = np.ascontiguousarray(a)
+
+        b = a.view(np.uint64)
+        b = b.reshape(*a.shape[:-1], 2)
+        return b
+
     # `tiles` is from your chunking function, shape (N, 4, 4, 7, 7)
     tiles_train = chunk_mnist_for_U1X(x_train)
     tiles_eval = chunk_mnist_for_U1X(x_test)
@@ -73,38 +80,41 @@ def dataset():
     tiles_train = recast_u64(tiles_train)
     tiles_eval = recast_u64(tiles_eval)
 
-    #all_tiles = np.unique(all_tiles_train, axis=0)
+    all_tiles_train = tiles_train.reshape(tiles_train.shape[0] * tiles_train.shape[1], tiles_train.shape[2])
+    all_tiles = np.unique(all_tiles_train, axis=0)
+    print(f"{all_tiles.shape[0]} total unique tiles")
+    # 1972878 tiles as is
 
-    #print(all_tiles.shape)
+    # we love setup being 4 seconds out of 28 second runtime on the poor laptop
 
-    #exit()
-
-    neur = U1XToU1X(np.empty(tiles_train.shape[2], tiles_train.dtype), cases=2000)
+    neur = U1XToU1X(np.empty(tiles_train.shape[2], tiles_train.dtype), cases=100_000) # case count inflated as chunking code was swapped for 7x7 tiles instead of 4x4
 
     counter = 0
+
     for tile in tiles_train:
         if counter % 1000 == 0:
             print(f"training at: {counter}")
-        _, rev = neur.forward(tile)
+        rev = neur.forward(tile)
         try:
             neur.assign(rev)
         except AssertionError:
-            print(f"Broke at {counter}")
-            break
-        else:
-            counter += 1
+            print(f"Broke at: {counter}, ran out out of cases\n")
+            raise
+        counter += 1
     print(f"\n\n{neur.array_used} cases used!\nMoving into validation\n")
+    # 811 as is
 
     counter = 0
     misses = 0
     for tile in tiles_eval:
         if counter % 1000 == 0:
-            print(f"training at: {counter}")
-        _, rev = neur.forward(tile)
+            print(f"eval at: {counter}")
+        rev = neur.forward(tile)
         misses += rev.shape[0]
         counter += 1
 
     print(f"\n\n{misses} inputs were unable to be mapped for eval")
+    # 0 as is
 
 
 
